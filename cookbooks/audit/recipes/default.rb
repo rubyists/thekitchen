1e61b825986c16b51427e93a638173bdfd13ba47
bougyman <bougyman@voidlinux.eu>
2015-10-08 07:19:33 -0500

    removed all of the cookbooks! moving to one-repo-per-cookbook with a chef-repo Berksfile

--- cookbooks/audit/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: audit
-# Recipe:: default
-#
-# Copyright (c) 2015 The Rubyists, All Rights Reserved.
-
-include_recipe 'audit::01_files'
-include_recipe 'audit::02_services'
--- cookbooks/securify/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-#
-# Cookbook Name:: securify
-# Recipe:: default
-#
-# Copyright (c) 2015 The Rubyists, All Rights Reserved.
-
-include_recipe 'securify::01_files'
